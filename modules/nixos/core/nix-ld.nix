64796f2828424d21a986f3346a82fc81641a4210
sshawn9 <29659937+sshawn9@users.noreply.github.com>
2026-06-27 03:50:05 +0800

    update nix-ld

--- modules/nixos/core/nix-ld.nix
+++ modules/nixos/core/nix-ld.nix
@@ -8,6 +8,26 @@
     enable = lib.mkDefault true;
     package = lib.mkDefault pkgs.unstable.nix-ld;
 
-    # libraries = with pkgs; [];
+    libraries = with pkgs; [
+      stdenv.cc.cc.lib
+
+      zlib
+      zstd
+      openssl
+      curl
+      libffi
+      sqlite
+
+      glib
+      dbus
+
+      libGL
+      fontconfig
+      freetype
+      libxkbcommon
+      wayland
+
+      libx11
+    ];
   };
 }
