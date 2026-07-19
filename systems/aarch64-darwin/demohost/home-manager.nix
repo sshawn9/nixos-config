869c3a4e23d7c7536f35c23185756af384093a85
sshawn9 <29659937+sshawn9@users.noreply.github.com>
2026-07-19 18:46:55 +0800

    feat: add demo configuration files for various situations

---
+++ systems/aarch64-darwin/demohost/home-manager.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  ...
+}:
+let
+  inherit (config.my.shared) username;
+in
+{
+  home-manager = {
+    users.${username} = {
+      home.username = username;
+    };
+  };
+}
---
+++ systems/aarch64-linux/demohost/hardware-configuration.nix
@@ -0,0 +1,8 @@
+_:
+
+{
+  fileSystems."/" = {
+    device = "none";
+    fsType = "tmpfs";
+  };
+}
