5e54f8a402acbb7b56ecc2b5b8c3e5cb6d58d3fb
sshawn9 <29659937+sshawn9@users.noreply.github.com>
2026-08-04 17:06:02 +0800

    feat(settings): add nix settings for extra substituters and trusted public keys for linux t2

---
+++ systems/x86_64-linux/nixos-a2141/t2/settings.nix
@@ -0,0 +1,13 @@
+_:
+
+{
+  nix.settings = {
+    extra-substituters = [
+      "https://cache.soopy.moe"
+    ];
+
+    extra-trusted-public-keys = [
+      "cache.soopy.moe-1:0RZVsQeR+GOh0VQI9rvnHz55nVXkFardDqfm4+afjPo="
+    ];
+  };
+}
